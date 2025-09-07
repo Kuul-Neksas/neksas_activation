8872d740c6e31eecfb88031906a56fa4c7c7a631
Kuul-Neksas <lmc.casillo@tiscali.it>
2025-09-07 19:59:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace('postgres://', 'postgresql://')
-    SUPABASE_URL = os.environ.get('SUPABASE_URL')
-    SUPABASE_ANON_KEY = os.environ.get('SUPABASE_ANON_KEY')
-    SUPABASE_JWT_SECRET = os.environ.get('SUPABASE_JWT_SECRET')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key')
+    SUPABASE_URL = os.environ.get('SUPABASE_URL', '')
+    SUPABASE_ANON_KEY = os.environ.get('SUPABASE_ANON_KEY', '')
+    SUPABASE_JWT_SECRET = os.environ.get('SUPABASE_JWT_SECRET', '')
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
